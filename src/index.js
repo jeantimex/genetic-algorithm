9b1e42d505de88fd3adc703cc967c96d585cbf34
Su <jean.timex@gmail.com>
2020-08-08 18:36:28 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import {
   clone,
-  encode,
-  decode,
   maximize,
   minimize,
   tournamentSelection,
@@ -23,32 +21,25 @@ const OptimizeStrategy = {
  */
 class GeneticAlgorithm {
   constructor(options = {}) {
-    const { optimizeStrategy, selectionStrategy } = options;
+    const { optimizeStrategy, selectionStrategy, seeds } = options;
 
     this.optimizeStrategy = optimizeStrategy || OptimizeStrategy.Maximize;
     this.selectionStrategy = selectionStrategy || SelectionStrategy.Tournament;
 
     this.fitness = null;
-    this.seed = null;
     this.mutate = null;
     this.crossover = null;
     this.generation = null;
-    this.notification = null;
+    this.onEvovle = null;
 
-    this.populationSize = 250;
     this.crossoverRate = 0.3;
     this.mutationRate = 0.3;
 
     this.iterations = 4000;
-    this.skip = 20;
-    this.maxResults = 100;
 
     this.fittestAlwaysSurvives = true;
 
-    this.userData = {};
-    this.internalGenState = {};
-
-    this.entities = [];
+    this.population = [];
   }
 
   get optimize() {
@@ -62,7 +53,7 @@ class GeneticAlgorithm {
 
   get selectSingle() {
     if (this.selectionStrategy === SelectionStrategy.Tournament) {
-      return tournamentSelection(this.optimize);
+      return tournamentSelection(this.fitness, this.optimize);
     }
   }
 
@@ -72,7 +63,65 @@ class GeneticAlgorithm {
     }
   }
 
-  evolve(i) {
+  get populationSize() {
+    return this.population.length;
+  }
+
+  evolve(generation) {
+    // score and sort by fitness
+    this.population
+      .sort((a, b) => {
+        // Put the fittest one in front of the array.
+        return this.optimize(this.fitness(a), this.fitness(b)) ? -1 : 1;
+      });
+
+    // generation notification
+    const mean =
+      this.population.reduce((a, b) => {
+        return a + this.fitness(b);
+      }, 0) / this.populationSize;
+    const stdev = Math.sqrt(
+      this.population
+        .map((a) => {
+          return (this.fitness(a) - mean) * (this.fitness(a) - mean);
+        })
+        .reduce((a, b) => {
+          return a + b;
+        }, 0) / this.populationSize
+    );
+
+    const stats = {
+      maximum: this.fitness(this.population[0]),
+      minimum: this.fitness(this.population[this.populationSize - 1]),
+      mean,
+      stdev,
+    };
+
+    const entities = this.population.map((entity) => ({
+      entity,
+      fitness: this.fitness(entity),
+    })); 
+
+    const r = this.isFinished
+      ? this.isFinished(
+        this.entities,
+        generation,
+        stats
+      )
+      : false;
+    const isFinished =
+      !!r ||
+      generation == this.iterations - 1;
+
+    this.onEvovle(
+      this.entities,
+      generation,
+      stats,
+      isFinished
+    );
+
+    if (isFinished) return true;
+
     const mutateOrNot = (entity) => {
       // applies mutation based on mutation probability
       return Math.random() <= this.mutationRate && this.mutate
@@ -80,124 +129,42 @@ class GeneticAlgorithm {
         : entity;
     }
 
-    // reset for each generation
-    this.internalGenState = {};
-
-    // score and sort
-    var pop = this.entities
-      .map((entity) => {
-        return { fitness: this.fitness(entity), entity: entity };
-      })
-      .sort((a, b) => {
-        return this.optimize(a.fitness, b.fitness) ? -1 : 1;
-      });
-
-    // generation notification
-    var mean =
-      pop.reduce((a, b) => {
-        return a + b.fitness;
-      }, 0) / pop.length;
-    var stdev = Math.sqrt(
-      pop
-        .map((a) => {
-          return (a.fitness - mean) * (a.fitness - mean);
-        })
-        .reduce((a, b) => {
-          return a + b;
-        }, 0) / pop.length
-    );
-
-    var stats = {
-      maximum: pop[0].fitness,
-      minimum: pop[pop.length - 1].fitness,
-      mean: mean,
-      stdev: stdev,
-    };
-
-    var r = this.generation
-      ? this.generation(
-          pop.slice(0, this.maxResults),
-          i,
-          stats
-        )
-      : true;
-    var isFinished =
-      (typeof r != 'undefined' && !r) ||
-      i == this.iterations - 1;
-
-    if (
-      this.notification &&
-      (isFinished ||
-        this.skip == 0 ||
-        i % this.skip == 0)
-    ) {
-      this.sendNotification(
-        pop.slice(0, this.maxResults),
-        i,
-        stats,
-        isFinished
-      );
-    }
-
-    if (isFinished) return true;
-
     // crossover and mutate
-    var newPop = [];
+    var newPopulation = [];
 
     if (this.fittestAlwaysSurvives)
       // lets the best solution fall through
-      newPop.push(pop[0].entity);
+      newPopulation.push(this.population[0]);
 
-    while (newPop.length < this.populationSize) {
+    while (newPopulation.length < this.populationSize) {
       if (
         this.crossoverRate && // if there is a crossover function
         Math.random() <= this.crossoverRate && // base crossover on specified probability
-        newPop.length + 1 < this.populationSize // keeps us from going 1 over the max population size
+        newPopulation.length + 1 < this.populationSize // keeps us from going 1 over the max population size
       ) {
-        var parents = this.selectParents(pop);
+        var parents = this.selectParents(this.population);
         var children = this.crossover(
           clone(parents[0]),
           clone(parents[1])
         ).map(mutateOrNot);
-        newPop.push(children[0], children[1]);
+        newPopulation.push(children[0], children[1]);
       } else {
-        newPop.push(mutateOrNot(this.selectSingle(pop)));
+        newPopulation.push(mutateOrNot(this.selectSingle(this.population)));
       }
     }
 
-    this.entities = newPop;
+    this.population = newPopulation;
   }
 
-  start() {
-    var i;
-
+  start(seeds) {
     // seed the population
-    this.entities = [];
-    for (i = 0; i < this.populationSize; ++i) {
-      this.entities.push(clone(this.seed()));
-    }
+    this.population = seeds;
 
-    for (i = 0; i < this.iterations; ++i) {
+    for (let i = 0; i < this.iterations; ++i) {
       if (this.evolve(i)) {
         break;
       }
     }
-  }
-
-  sendNotification(pop, generation, stats, isFinished) {
-    const response = {
-      pop: pop.map(encode),
-      generation: generation,
-      stats: stats,
-      isFinished: isFinished,
-    };
-
-    this.notification(
-      response.pop.map(decode),
-      response.generation,
-      response.stats,
-      response.isFinished
-    );
   }
 }
 
